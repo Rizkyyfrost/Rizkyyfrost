25e04024145202d54671b400303bf60891f2abe4
Rizkyyfrost <nackepep8@gmail.com>
2024-09-15 22:37:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+      
+     document.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); 
+    });
+  
+
+
+   document.querySelector('.verse').innerHTML = `
+         <p class="arabic">وَتَمَّتْ كَلِمَتُ رَبِّكَ صِدْقًا وَّعَدْلًاۗ لَا مُبَدِّلَ لِكَلِمٰتِهٖ ۚوَهُوَ السَّمِيْعُ الْعَلِيْمُ </p>
+        <p>Telah sempurna kalimat Tuhanmu (Al-Qur’an) dengan (mengandung) kebenaran dan keadilan. Tidak ada (seorang pun) yang dapat mengubah kalimat-kalimat-Nya. Dia Maha Mendengar lagi Maha Mengetahui.</p>
+        <p><em class="source">Source: Al'Anam, Ayat 115</em></p>
+        `;
+
+async function getVerse() {
+
+            
+
+    const response = await fetch('./content.json');
+    const data = await response.json();
+
+
+    const mood = document.getElementById('mood').value;
+    const filteredVerses = data.verses.filter(verse => verse.mood === mood);
+
+    if (filteredVerses.length > 0) {
+        const randomIndex = Math.floor(Math.random() * filteredVerses.length)
+        const verseData = filteredVerses[randomIndex];
+
+        document.querySelector('.verse').innerHTML = `
+        
+        <p class="arabic">${verseData.arabic}</p>
+        <p>${verseData.verse}</p>
+        <p><em class="source">Source: ${verseData.source}</em></p>
+        `;
+
+    } else {
+        document.querySelector('.verse').innerHTML = `<p>Coba lagi</p>`;
+
+    }
+
+    
+}
+
+
+
